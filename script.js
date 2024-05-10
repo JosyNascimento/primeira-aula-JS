5bad9a8b70a09aec2a28986f750cd836fba81f87
Joseilda Nascimento <joseildatn@gmail.com>
2024-05-09 21:20:34 -0300

    aula1

---
+++ script.js
@@ -0,0 +1,29 @@
+
+            var nome = "Joseilda";
+            var sobrenome = "Nascimento";
+            var idade = 39;
+            var anoAtual = 2024;
+            var nomeEsobrenome = nome + sobrenome
+            var anoNascimento = anoAtual - idade
+           
+        console.log (`nome completo: ${nomeEsobrenome}`)
+        console.log (`Ano de nascimento: ${anoNascimento}`)
+
+     
+
+
+
+        var idade1 = 40;
+        var idade2 = 15;
+        var resultado = idade1 + idade2
+
+        console.log ('idade1Eidade2=' + resultado)
+
+
+//atribuição
+
+
+
+//inicializar variável
+
+var aula ="aula 2"
